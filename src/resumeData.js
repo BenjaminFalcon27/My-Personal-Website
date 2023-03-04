a7fcc2f4ba6113455b530ed4251479ae6061cdb7
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2023-03-04 21:46:17 +0100

    add: projects and skills css

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,117 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://benjaminfalcon27.github.io/",
+  name: "Benjamin",
+  role: "Junior Web Developer",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/benjamin-trolais-2471a623b/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/benjaminfalcon27",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "As a highly enthusiastic and dedicated beginner web developer, I am eager to start my career in the field and bring my passion for technology to life. I have a solid understanding of RoR, Javascript, React and I am constantly seeking new opportunities to expand my skillset.",
+  address: "Lille, France",
+  website: "https://benjaminfalcon27.github.io",
+  education: [
+    {
+      UniversityName: "IUT Calais",
+      specialization: "LP DIM (Web and Mobile Development)",
+      MonthOfPassing: "",
+      YearOfPassing: "2022 - 2023",
+      Achievements: "ReactJS, ReactNative, Flutter, Javascript",
+    },
+    {
+      UniversityName: "IUT Calais",
+      specialization: "DUT IT",
+      MonthOfPassing: "",
+      YearOfPassing: "2020 - 2022",
+      Achievements: "PHP, Java, C, Python, SQL, HTML, CSS, Javascript",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Octopus Lab",
+      specialization: "Apprenticeship as a Full Stack Web Developer",
+      MonthOfLeaving: "",
+      YearOfLeaving: "2022 - 2023",
+      Achievements:
+        "Ruby on Rails, HAML, SCSS, Git, Bootstrap, RSpec, Docker, Kubernetes",
+    },
+    {
+      CompanyName: "Translation.io",
+      specialization: "Internship as a Full Stack Web Developer",
+      MonthOfLeaving: "Apr - Jun",
+      YearOfLeaving: "2020",
+      Achievements: "Ruby on Rails, Git, ES6, RSpec, Capybara",
+    },
+  ],
+  skillsDescription: "My skills",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Ruby on Rails",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "PHP",
+    },
+    {
+      skillname: "Java",
+    },
+    {
+      skillname: "C",
+    },
+    {
+      skillname: "Python",
+    },
+    {
+      skillname: "SQL",
+    },
+  ],
+  portfolio: [
+    {
+      name: "React Memory Card Game",
+      description: "My first React project",
+      imgurl: "images/portfolio/yoschi.jpg",
+      url: "https://memory-card-game-rust.vercel.app/",
+    },
+    {
+      name: "RoR Todo List",
+      description: "My first React and Rails project",
+      imgurl: "images/portfolio/Todolist.jpg",
+      url: "https://github.com/BenjaminFalcon27/todolist",
+    },
+    {
+      name: "League of Legends List in React Native",
+      description:
+        "List of all champions in League of Legends in React Native with API call",
+      imgurl: "images/portfolio/mobile app.jpg",
+      url: "https://expo.dev/@benjaminfalcon27/league-des-legendes",
+    },
+    {
+      name: "Next Project",
+      description: "This project will come soon",
+      imgurl: "images/portfolio/Coming soon.png",
+      url: "",
+    },
+  ],
+};
+
+export default resumeData;
